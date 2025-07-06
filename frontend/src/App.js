828f9055d12cd0255b574185f486b6987caaaf03
Brick McDermott <82049931+mikdermit@users.noreply.github.com>
2025-07-06 11:17:34 -0400

    updating files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <Switch>
+            <Route path="/">
+                <Layout />
+            </Route>
+        </Switch>
+    );
 }
 
 export default App;
