ce223aa3d3ed67f7956e00679853e6daf71937bc
Brick McDermott <82049931+mikdermit@users.noreply.github.com>
2025-07-27 18:33:45 -0400

    updates

--- front-end/src/components/donate/Donate.js
+++ front-end/src/components/donate/Donate.js
@@ -15,18 +15,18 @@ function Donate() {
                 <h1>Donate</h1>
             </div>
             <div class="container">
-                <div class="d-flex justify-content-around flex-wrap">
-                    
-                        <button type="button" class="btn btn-primary mt-4 w-25">
-                            <i class="fa-brands fa-paypal pe-3" />
-                            Paypal
-                        </button>
-                
-                    <button type="button" class="btn btn-warning mt-4 w-25">
+                <div class="d-flex flex-column flex-md-row justify-content-around align-items-center flex-wrap">
+
+                    <button type="button" class="btn btn-primary mt-4" style={{width:'fit-content', minWidth:'35%'}}>
+                        <i class="fa-brands fa-paypal pe-3" />
+                        Paypal
+                    </button>
+
+                    <button type="button" class="btn btn-warning mt-4" style={{width:'fit-content', minWidth:'35%'}}>
                         <i class="fa-brands fa-amazon pe-3"/>
                         Amazon Wishlist
                     </button>
-                    <button type="button" class="btn btn-primary mt-4 w-25">
+                    <button type="button" class="btn btn-primary mt-4" style={{width:'fit-content', minWidth:'35%'}}>
                         <i class="fa-solid fa-cat pe-3"/>
                         Chewy Wishlist
                     </button>
